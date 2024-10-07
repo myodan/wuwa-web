d0a8181b19792bc403a5e6b7dc635b7e4de3f46b
Jongho Hong <myodan@pm.me>
2024-10-07 17:14:26 +0900

    chore: Update package.json and layout.tsx
    Update package.json to include the "@vercel/analytics" dependency.
    Add the <Analytics /> component to the layout.tsx file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import { Noto_Sans_KR } from "next/font/google";
@@ -31,6 +32,7 @@ const RootLayout = ({ children }: Props) => {
 					{children}
 				</Provider>
 				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	);
